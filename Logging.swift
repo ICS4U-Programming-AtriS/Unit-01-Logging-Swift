7c6bfd208930900ae06e113943bb3abda2e19ed4
Atri Sarker <atri.sarker@stu.ocsb.ca>
2025-09-17 15:29:37 +0000

    Fixed Linter errors

--- Logging.swift
+++ Logging.swift
@@ -10,24 +10,25 @@
 // This software is used to calculate the amount of logs that a truck can fit.
 // Given input for a log length, it will return the log amount.
 
+// NOTE: CAN'T USE _ TO NAME CONSTANTS
 // Maximum payload capacity for the truck. [IN KG]
-let TRUCK_WEIGHT_LIMIT = 1100
+let TRUCKWEIGHTLIMIT = 1100
 // Rate that defines how much a log weighs per meter. [IN KG/M]
-let WEIGHT_PER_METER = 20
+let WEIGHTPERMETER = 20
 // Our company's offered log lengths.
 // Only these lengths are offered due to ease of inventory and transport.
 // More log lengths may be offered in the future.
-let LOG_LENGTHS = [0.25, 0.5, 1]
+let LOGLENGTHS = [0.25, 0.5, 1]
 
 // Welcome message.
 print("Welcome to the ATREE logging company's ", terminator: "")
 print("tooling kit for industry-standard log packing.")
 print("Our software is known to be robust ", terminator: "")
-print("and military-grade, much like our logs.");
-print();
+print("and military-grade, much like our logs.")
+print()
 // Prompt for log length.
 print("LOG LENGTHS (m): [ | ", terminator: "")
-for length in LOG_LENGTHS { print("\(length) ", terminator: " | ") }
+for length in LOGLENGTHS { print("\(length) ", terminator: " | ") }
 print("]")
 print("Enter the length of the log (m): ", terminator: "")
 // Get log length as a string.
@@ -35,9 +36,9 @@ if let logLengthAsString = readLine() {
     // Convert log length input to a double.
     if let logLength = Double(logLengthAsString) {
         // Check if the log length is a valid length
-        if LOG_LENGTHS.contains(logLength) {
+        if LOGLENGTHS.contains(logLength) {
             // Calculate the amount of logs that can fit in the truck
-            let logAmount = Double(TRUCK_WEIGHT_LIMIT) / (Double(WEIGHT_PER_METER) * logLength)
+            let logAmount = Double(TRUCKWEIGHTLIMIT) / (Double(WEIGHTPERMETER) * logLength)
             // Display the result [IN BLUE]
             print("\u{001B}[0;34mLog Amount : ", terminator: "")
             print("\(Int(logAmount))")
